c51ea37f9ccd3357bbd7c0851b667a1423edbfe2
huykingsofm <huykingsofm@gmail.com>
2024-10-12 02:38:36 +0700

    fix

--- domain/oauth2_scope.go
+++ domain/oauth2_scope.go
@@ -11,9 +11,9 @@ var ScopeEngine = scope.NewEngine(actionMap, resourceMap)
 
 func init() {
 	// Full permission
-	ScopeEngine.Define(Actions, Resources)
-	ScopeEngine.Define(Actions.Read, Resources)
-	ScopeEngine.Define(Actions.Write, Resources)
+	ScopeEngine.Define(Actions, Resources)       // *
+	ScopeEngine.Define(Actions.Read, Resources)  // read
+	ScopeEngine.Define(Actions.Write, Resources) // write
 
 	// User
 	ScopeEngine.Define(Actions.Read, Resources.User).Description("grant read-only access to user information")
@@ -23,6 +23,7 @@ func init() {
 	// OAuth2 Client
 	ScopeEngine.Define(Actions.Read, Resources.Client).Description("grant read-only access to client's information")
 	ScopeEngine.Define(Actions.Read, Resources.Client.AllowedScope).Description("grant read-only access to client's allowed scope")
+
 	ScopeEngine.Define(Actions.Write, Resources.Client).Description("grant write permission to OAuth2 Client")
 	ScopeEngine.Define(Actions.Write.Create, Resources.Client).Description("allow user to create OAuth2 Client")
 }
